ec48a6d00f3a8b1302a554a5dad0382bbc4e4c4b
bhargavigottumukkala2005 <154610392+bhargavigottumukkala2005@users.noreply.github.com>
2024-06-01 22:13:03 +0530

    Create lcm5.c

---
+++ lcm5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int result = 1;
+    for (int i = 2; i <= 20; i++) {
+        result = lcm(result, i);
+    }
+    printf("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is %d\n", result);
+    return 0;
+}
