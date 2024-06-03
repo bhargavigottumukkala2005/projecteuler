d0db37f2498048f1b478bb64ad43ce31d3eb8f63
bhargavigottumukkala2005 <154610392+bhargavigottumukkala2005@users.noreply.github.com>
2024-06-03 17:34:15 +0530

    Create Sumsquaredifference6.c

---
+++ Sumsquaredifference6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int sumofsquares = 0;
+    int squareofsum = 0;
+    int i;
+
+    for (i = 1; i <= 100; i++) {
+        sumofsquares += i * i;
+    }
+
+    int sum = 0;
+    for (i = 1; i <= 100; i++) {
+        sum += i;
+    }
+    squareofsum = sum * sum;
+
+    int dif = squareofsum - sumofsquares;
+
+    printf("Difference between the sum of the squares and the square of the sum for the first 100 natural numbers is: %d\n", dif);
+
+    return 0;
+}
