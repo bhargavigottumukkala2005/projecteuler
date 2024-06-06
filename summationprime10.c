947a0d684805031ebeb3dbe05a5c638b69a4232f
bhargavigottumukkala2005 <154610392+bhargavigottumukkala2005@users.noreply.github.com>
2024-06-06 16:47:02 +0530

    Create summationprime10.c

---
+++ summationprime10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int is_prime(int num) {
+    if (num < 2) return 0;
+    int count = 0;
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0) count++;
+        if (count > 1) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    long long sum = 0;
+
+    for (int num = 2; num < 2000000; num++) {
+        if (is_prime(num)) {
+            sum += num;
+        }
+    }
+
+    printf("The sum of all primes below two million is %lld\n", sum);
+    return 0;
+}
